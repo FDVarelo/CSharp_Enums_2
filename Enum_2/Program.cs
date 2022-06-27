59462caab70b890aa7007dc50c2b929f0d63d8ec
FDVarelo <fdvarelo@gmail.com>
2022-06-27 08:50:02 -0300

    first commit

---
+++ Enum_2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+
+namespace Enum_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
